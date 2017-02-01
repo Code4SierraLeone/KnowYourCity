e89b03a25802821beb9f1a40459fec56d46a8577
samsonpaul <samsonpaul4@gmail.com>
2017-02-01 21:21:03 +0200

    Switch server to TLS

--- app/main.go
+++ app/main.go
@@ -1,64 +1,100 @@
-// The main binary is the KnowYourCity server.
 package main
 
 import (
-	"log"
-	"flag"
-	"net/http"
-
-
-	"gopkg.in/mgo.v2"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
+	"flag"
+	"fmt"
+	"gopkg.in/mgo.v2"
 	"github.com/gorilla/securecookie"
+	"net/http"
+	"log"
+	//	"reflect"
 )
 
-var hashKey = securecookie.GenerateRandomKey(32)
-var blockKey = securecookie.GenerateRandomKey(32)
-var sc = securecookie.New(hashKey, blockKey)
+// If content-type not set manually, it will be guessed by http://golang.org/src/pkg/net/http/sniff.go
+// w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// io.WriteString(w, v)
+// io.WriteString(w, `<br><form method="POST" action="/post"><input name="s"></form>`)
+// io.WriteString(w, `<div>hello</div>`)
 
-// Configures variables
-var (
-	serveraddr string
-)
+// HTTP is stateless: a client computer running a web browser must establish a new TCP network connection to the web server with each new HTTP GET or POST request. The web server, therefore, cannot rely on an established TCP network connection for longer than a single HTTP GET or POST operation.
+// Session management is the technique used by the web developer to make the stateless HTTP protocol support session state. For example, once a user has been authenticated to the web server, the user's next HTTP request (GET or POST) should not cause the web server to ask for the user's account and password again. For a discussion of the methods used to accomplish this please see HTTP cookie.
+// http://en.wikipedia.org/wiki/Session_management
+// http://en.wikipedia.org/wiki/HTTP_cookie
+var store = sessions.NewCookieStore([]byte("something-very-secret"))
 
-// main
+var addr = flag.String("addr", ":9004", "http service address") // Q=17, R=18
+
 func main() {
-	serveraddr = *flag.String("srv", ":9004", "-srv=addr; set server listening address.Default :9004")
+	store.Options.Secure = true
 	flag.Parse()
+	// If only one version can be returned (i.e., the other redirects to it), that’s great!
+	// http://googlewebmastercentral.blogspot.com/2010/04/to-slash-or-not-to-slash.html
+	r := mux.NewRouter().StrictSlash(true)
+	// If a site is accessed over HTTPS and loads some parts of a page over insecure HTTP, the user might still be vulnerable to some attacks or some kinds of surveillance. For instance, the New York Times makes many HTML pages available in HTTPS, but other resources such as images, CSS, JavaScript, or third party ads and tracking beacons, are only loadable over HTTP. That means that these resources are sent unencrypted, and someone spying on you could probably infer the article you were reading.
+	// There are also potential vulnerabilities when parts of a page are loaded over HTTP because an attacker might replace them with versions containing false information, or Javascript code that helps the attacker spy on the user or take over an account.
+	//
+	// In order to [enable HTTPS by default for GMail] we had to deploy
+	// no additional machines and no special hardware. On our production
+	// frontend machines, SSL/TLS accounts for less than 1% of the CPU load, less
+	// than 10KB of memory per connection and less than 2% of network overhead.
+	// www.eff.org/https-everywhere/faq
 
-	dbSession, err := dialDB()
-	if err != nil {
-		log.Fatal(err)
+	// no referral with https?
+
+	//Not needed, because there is redirecting
+	//s := r.Schemes("https").Sub-router()
+	r.HandleFunc("/", http.HandlerFunc(index))
+	// REST good practice: trailing slash denotes a directory, while the lack of it denotes a file/resource
+	// http://techblog.bozho.net/?p=401
+	r.HandleFunc("/login", http.HandlerFunc(Login))
+	r.HandleFunc("/register", http.HandlerFunc(Register))
+	r.HandleFunc("/password/reset", http.HandlerFunc(ResetPassPage))
+	r.HandleFunc("/password/reset", http.HandlerFunc(ResetPass))
+	r.HandleFunc("/password/forgot", http.HandlerFunc(ForgotPass))
+	r.HandleFunc("/2factor", http.HandlerFunc(L2Factor))
+	r.HandleFunc("/logout", http.HandlerFunc(Logout))
+	r.HandleFunc("/code/get", http.HandlerFunc(GetSecretCode))
+	r.HandleFunc("/code/set", http.HandlerFunc(SetSecretCode))
+
+
+	http.Handle("/", r)
+	fs := JustFilesFilesystem{http.Dir("static/")}
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(fs)))
+
+	// starting in goroutines with error reporting, thanks to davecheney from #go-nuts
+	// like this: http://serverfault.com/questions/67316/in-nginx-how-can-i-rewrite-all-http-requests-to-https-while-maintaining-sub-dom
+	// It will direct from http://localhost/users to https://localhost:9999/users, but not from http://localhost:9999/users
+	go func() {
+		log.Fatalf("ListenAndServe: %v", http.ListenAndServe(":8080", http.HandlerFunc(notlsHandler)))
+	}()
+	go func() {
+		fmt.Println("https://localhost"+*addr)
+		// go run generate_cert.go --host localhost
+		// this works for other domain than localhost, no need to supply domain name:
+		// openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes
+		// http://stackoverflow.com/questions/10175812/how-to-build-a-self-signed-certificate-with-openssl
+		log.Fatalf("ListenAndServeTLS: %v", http.ListenAndServeTLS(*addr, "tls/cert.pem", "tls/key.pem", nil))
+	}()
+	select {}
+
+	// Testing redirecting
+	// curl -v http://localhost:8080/products
+	// Or if you made forwarding or run as superuser so you have access to ports below 1024
+	// curl -v http://localhost/products
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if r.Method != "GET" || r.URL.Path != "/" {
+		serve404(w)
+		return
 	}
 
-	router := mux.NewRouter()
-	setAccountRoutes(router, dbSession)
+	pageTemplate.Execute(w, tplValues)
+	if err != nil {
+		serveError(w, err)
+	}
 
-	// TODO: Switch to TLS in prod(samson)
-	http.ListenAndServe(serveraddr, router)
 }
-
-// dialDB
-func dialDB() (*mgo.Session, error) {
-	return mgo.Dial("localhost")
-}
-
-// setAccountRoutes
-// REST good practics: trailing slash denotes a directory, while the lack of it denotes a file/resource
-func setAccountRoutes(router *mux.Router, dbSession *mgo.Session) {
-	router.HandleFunc("/account/register", withCORS(withClient(DbSession(db, SecretCode(Register))))).Methods("POST")
-	router.HandleFunc("/account/login", withCORS(withClient(DbSession(db, Login)))).Methods("POST")
-	router.HandleFunc("/account/password/forgot", withCORS(withClient(DbSession(db, ForgotPass)))).Methods("POST")
-	router.HandleFunc("/page/account/password/reset", withCORS(withClient(DbSession(db, ResetPassPage)))).Methods("POST")
-	router.HandleFunc("/account/password/reset", withCORS(withClient(DbSession(db, ResetPass)))).Methods("POST")
-	router.HandleFunc("/account/2factor", withCORS(withClient(DbSession(db, L2Factor)))).Methods("POST")
-	router.HandleFunc("/account/logout", withCORS(withClient(DbSession(db, Logout)))).Methods("POST")
-
-	router.HandleFunc("/keys/code/get", withCORS(withClient(DbSession(dbSession, GetSecretCode))))
-	router.HandleFunc("/keys/code/set", withCORS(withClient(DbSession(dbSession, SetSecretCode))))
-}
-
-
-/*
-*/
-
